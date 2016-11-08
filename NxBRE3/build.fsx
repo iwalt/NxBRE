aa43d02c2b04b4bd69fc9d4fa54c75c3383be81e
Ian Walters <ian.walters@rpsgroup.com>
2016-11-08 18:02:02 +0000

    Added NuGet package creation to build-script.

--- NxBRE3/build.fsx
+++ NxBRE3/build.fsx
@@ -1,36 +1,75 @@
-// include Fake libs
 #r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
 
-// Directories
+
+// Project information.
+let authors = ["david@dossot.net"]
+let projectName = "NxBRE"
+let projectDescription = "Lightweight Business Rules Engine for .NET"
+let projectSummary = projectDescription
+
+
+// Tools.
+
+// TODO: Would be better if this was restored into project.
+let nugetPath = ProgramFilesX86 @@ "NuGet/nuget.exe"
+
+
+// Directories.
 let buildDir  = "./build/"
 let deployDir = "./deploy/"
+let packagingDir = "./packaging/"
 
 
-// Filesets
+// Filesets.
 let appReferences = !! "/**/NxBRE.csproj"
 
-// Targets
-Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
-)
+
+// Targets.
+
+Target "Clean" (fun _ -> CleanDirs [buildDir; deployDir; packagingDir])
 
 Target "Build" (fun _ ->
-    MSBuildDebug buildDir "Build" appReferences
-    |> Log "AppBuild-Output: "
-)
+    MSBuildDebug buildDir "Build" appReferences |> Log "AppBuild-Output: ")
 
 Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-    -- "*.zip"
-    |> Zip buildDir (deployDir + "NxBRE3.zip")
+    !! (buildDir + "/**/*.*") -- "*.zip"
+        |> Zip buildDir (deployDir + "NxBRE.zip"))
+
+Target "CreatePackage" (fun _ ->
+
+    let net451Dir = packagingDir @@ "lib/net451/"
+    CleanDirs [net451Dir]
+
+    CopyFile net451Dir (buildDir @@ "NxBRE.dll")
+    CopyFile net451Dir (buildDir @@ "NxBRE.pdb")
+    CopyFile packagingDir "README.txt"
+    CopyFile packagingDir "MIT-LICENSE.txt"
+
+    let version = (net451Dir @@ "NxBRE.dll") |> GetAssemblyVersionString
+
+    NuGet
+        (fun p ->
+            { p with
+                Authors = authors
+                Project = projectName
+                Description = projectDescription
+                OutputPath = packagingDir
+                Summary = projectSummary
+                WorkingDir = packagingDir
+                Version = version
+                Publish = false
+                ToolPath = nugetPath
+            })
+        "NxBRE.nuspec"
 )
 
-// Build order
-"Clean"
-  ==> "Build"
-  ==> "Deploy"
 
-// start build
+// Build order.
+"Clean" ==> "Build" ==> "Deploy"
+"Clean" ==> "Build" ==> "CreatePackage"
+
+
+// Start build.
 RunTargetOrDefault "Build"
